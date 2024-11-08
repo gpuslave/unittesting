d6e067f4f8757ad5017419d6f350e9b66d80232b
Alex Nemt <alenemt@yandex.ru>
2024-11-08 10:04:58 +0300

    initialize Actions

--- test/model_testing.py
+++ test/model_testing.py
@@ -1,12 +1,13 @@
-import os, sys
+import os
+import sys
 import unittest
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.svm import LinearSVR
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(current_dir)
-sys.path.append(parent_dir)
+# current_dir = os.path.dirname(os.path.abspath(__file__))
+# parent_dir = os.path.dirname(current_dir)
+# sys.path.append(parent_dir)
 
 from unittesting.master import ModelTraining
 
